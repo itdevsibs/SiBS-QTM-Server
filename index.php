e59e3443b63e5dd126a00fbf151b7c25560f6a75
SIBS\dexchaunceyy <dexchauncey.ybanez@thesiblingssolutions.com>
2025-09-24 16:33:40 +0800

    CORS edits

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/database/dbconnect.php';
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
 
 // Create App
 $app = AppFactory::create();
@@ -15,7 +19,6 @@ $app->addBodyParsingMiddleware();
 
 // Add CORS middleware
 $app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
 
     $origin = $request->getHeaderLine('Origin'); // get the Origin header/URL from the request
     $allowedOrigins = [
@@ -24,7 +27,10 @@ $app->add(function ($request, $handler) {
         'https://testreact.getleadsource.com' /* staging URL */
     ]; //allowed origins
 
-    if(in_array($origin, $allowedOrigins)) {
+    $isOptions = strtoupper($request->getMethod()) === 'OPTIONS';
+    $response = $isOptions ? new \Slim\Psr7\Response(204) : $handler->handle($request);
+
+    if(in_array($origin, $allowedOrigins, true)) {
         $response = $response
         ->withHeader('Access-Control-Allow-Origin', $origin)        
         ->withHeader('Access-Control-Allow-Credentials', 'true');
@@ -32,7 +38,15 @@ $app->add(function ($request, $handler) {
 
     $response = $response
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        ->withHeader('Access-Control-Expose-Headers', 'Authorization')
+        ->withHeader('Vary', 'Origin');
+
+    // Cache preflight response for 1 day unless in development or dockerprod environment
+    if ($_ENV['app_env'] !== 'development' && $_ENV['app_env'] !== 'dockerprod') {
+        $response = $response->withHeader('Access-Control-Max-Age', '86400');
+    }
+
     return $response;
 });
 
